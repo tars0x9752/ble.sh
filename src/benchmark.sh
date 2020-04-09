98768fdab6b811c33e77655a28fbc3671ab76f62
Koichi Murase <myoga.murase@gmail.com>
2020-04-09 18:51:07 +0900

    osh: use OIL_VERSION for shell detection

--- src/benchmark.sh
+++ src/benchmark.sh
@@ -44,7 +44,7 @@ if [[ ${ZSH_VERSION-} ]]; then
       return 1
     fi
   }
-elif [[ $0 != osh ]] && ((BASH_VERSINFO[0]>=5)); then
+elif [[ ! $OIL_VERSION ]] && ((BASH_VERSINFO[0]>=5)); then
   _ble_measure_resolution=1 # [usec]
   function ble-measure/.get-realtime {
     local LC_ALL= LC_NUMERIC=C
